9becbcccafc1825ce6b2467e21a8ae7d77f38a14
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-20 01:41:49 +0300

    [new]: provides stas about nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+provides some stats about nginx logs stored in mongoDB
+"""
+from pymongo import MongoClient
+
+
+def get_stats():
+    """
+    gets some stats from nginx logs
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+
+    print(f"{nginx.count_documents({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {nginx.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {nginx.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {nginx.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {nginx.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {nginx.count_documents({'method': 'DELETE'})}")
+    query = {"method": "GET", "path": "/status"}
+    print(f"{nginx.count_documents(query)} status check")
+
+
+if __name__ == "__main__":
+    get_stats()
