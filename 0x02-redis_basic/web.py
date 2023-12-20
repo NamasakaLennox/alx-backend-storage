30912ae87e734a9fc4189a77eba26fbdbf63ebe8
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-20 22:57:15 +0300

    [fix]: fixed method

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,29 +4,31 @@ obtain the HTML content of a particular URL and returns it
 """
 import requests
 import redis
+from typing import Callable
 from functools import wraps
 
 store = redis.Redis()
 
 
-def cout_access(method):
+def cout_access(method: Callable) -> Callable:
     """
     decorator to count how many times a url is accessed
     """
     @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+    def wrapper(url) -> str:
+        """
+        wrapper function for caching
+        """
+        store.incr(f'count:{url}')
+        result = store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
 
-        count_key = "count:" + url
-        html = method(url)
+        result = method(url)
+        store.set(f'count:{url}', 0)
+        store.setex(f'result:{url}', 10, result)
+        return result
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
     return wrapper
 
 
