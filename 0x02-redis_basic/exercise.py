119b489db206eb7e1faf27df9bc5653c4b7eccf1
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-20 19:42:32 +0300

    [new]: creates a new object and adds to store

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+module for implementing redis in python
+"""
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """
+    A cache store class
+    """
+    def __init__(self):
+        """
+        class constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        takes a data argument and returns a string
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+
+        return key
